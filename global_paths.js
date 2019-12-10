d3af4ba8ce891781e5a4d5132f473de233b3fd4c
sang.nguyen <nguyentansang1996@gmail.com>
2019-12-10 14:26:57 +0700

    [update & fix]: global path and fix some bugs

--- global_paths.js
+++ global_paths.js
@@ -6,11 +6,14 @@ const path = require('path')
 
 // Root path
 global.APP_ROOT_PATH = `${__dirname}/app`
+global.APP_COMMON_PATH = `${__dirname}/utils`
 
 // Set global config
 global.APP_ROUTE_PATH = `${global.APP_ROOT_PATH}/route`
 global.APP_CONTROLLER_PATH = `${global.APP_ROOT_PATH}/controller`
-
+global.APP_MIDDLEWARE_PATH = `${global.APP_ROOT_PATH}/middleware`
+global.APP_MANAGER_PATH = `${global.APP_ROOT_PATH}/manager`
+global.APP_BASE_PACKAGE_PATH = `${global.APP_ROOT_PATH}/base`
 // related resources
 
 /***************************** EXAMPLE **********************************************
--- index.js
+++ index.js
@@ -5,12 +5,17 @@
 // Set app environment
 require('./config/.env')
 // Set other app paths
-require('./global-paths')
+require('./global_paths')
 // Set config variables
 global.CONFIG = require(`./config/${process.app_env}`)
-
+const { port, sslPort } = global.CONFIG.app
+const dbConfig = global.CONFIG.db
 const express = require('express')
 const app = express()
+const http = require('http')
+const https = require('https')
+const mongoose = require('mongoose')
+
 const cors = require('cors')
 // import route path
 const routes = require(global.APP_ROUTE_PATH)
@@ -22,7 +27,62 @@ app.use('/api', routes)
 // default route
 app.use('/', (req, res) => {
   res.statusCode = 200 // send the appropriate status code
-  res.json({ status: 'success', message: 'WELCOME TO BACKEND' })
+  res.json({status: 'success', message: 'WELCOME TO BACKEND'})
 })
 
+const secureServer = https.createServer({}, app)
+const server = http.createServer(app)
+server.on('listening', onListening)
+server.on('error', onError)
+secureServer.on('listening', onListening)
+secureServer.on('error', onError)
+
+// Connect to DB
+mongoose.Promise = global.Promise
+mongoose.connect(dbConfig.url, dbConfig.options)
+  .then(() => {
+    console.log('db connected: ', dbConfig)
+    server.listen(port)
+    secureServer.listen(sslPort)
+  })
+  .catch(error => {
+    console.log('connect db error: ', error)
+  })
+mongoose.set('debug', true)
+
 module.exports = app
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error('requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error('port is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`
+
+  console.log('Server listenning on port:', addr.port)
+}
