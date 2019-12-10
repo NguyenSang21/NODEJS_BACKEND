6245d118f190a7486f5398043ce84e6f29c1c2b3
sang.nguyen <nguyentansang1996@gmail.com>
2019-12-10 14:29:31 +0700

    [update]: manager

--- app/manager/rest.manager.js
+++ app/manager/rest.manager.js
@@ -1,23 +1,13 @@
-
 const BaseManager = require(`${global.APP_MANAGER_PATH}/base.manager`)
-const I18nLogic = require(`${global.CORE_LOGIC_PATH}/i18n.bis`)
-const i18n = require(`${global.CORE_COMMON_PATH}/i18n`)
 
 class RestManager extends BaseManager {
   constructor(mongoose) {
     super()
-    this.i18nLogic = new I18nLogic(mongoose)
-  }
-
-  getI18nMessage(key, lang) {
-    if (key) {
-      return this.i18nLogic.getMessage(key, lang)
-    }
-    return Promise.resolve()
   }
 
   responseWithError(res, err, code) {
-    const sendData = { success: false }
+    const sendData = {success: false}
+
     if (typeof err === 'object' && typeof err.message !== 'undefined') {
       sendData.message = err.message
     } else if (typeof err === 'string') {
@@ -26,23 +16,11 @@ class RestManager extends BaseManager {
 
     if (typeof code !== 'undefined') res.statusCode = code
 
-    const i18nKey = sendData.message
-    this.getI18nMessage(i18nKey, res._lang)
-      .then(message => {
-        if (message) {
-          sendData.message = message
-        } else {
-          sendData.message = i18n.defaultMessage(i18nKey)
-        }
-
-        res.json(sendData)
-      }).catch(() => {
-        res.json(sendData)
-      })
   }
 
   responseWithSuccess(res, data, code) {
-    let sendData = { success: true }
+    let sendData = {success: true}
+
     if (typeof data === 'object') {
       sendData = Object.assign(data, sendData)// merge the objects
     } else if (typeof data === 'string') {
@@ -51,14 +29,11 @@ class RestManager extends BaseManager {
 
     if (typeof code !== 'undefined') res.statusCode = code
 
-    this.getI18nMessage(sendData.message, res._lang)
-      .then(message => {
-        if (message) sendData.message = message
-
-        res.json(sendData)
-      }).catch(() => {
-        res.json(sendData)
-      })
+    if (Array.isArray(sendData)) {
+      res.set('Access-Control-Expose-Headers', 'X-Total-Count')
+      res.set('X-Total-Count', data.total || 0)
+    }
+    res.json(sendData)
   }
 }
 
